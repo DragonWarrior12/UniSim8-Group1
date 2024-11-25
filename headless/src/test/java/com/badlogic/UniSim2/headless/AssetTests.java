aec0f4d66f01e7bc1b63d7e3080bb48c106d4d51
Dragon Warior <evelyn.gravett@protonmail.com>
2024-11-25 15:22:11 +0000

    add jacoco, junit, and mockito; implement asset tests

---
+++ headless/src/test/java/com/badlogic/UniSim2/headless/AssetTests.java
@@ -0,0 +1,17 @@
+package com.badlogic.UniSim2.headless;
+
+import com.badlogic.UniSim2.resources.Assets;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AssetTests extends AbstractHeadlessGdxTest {
+    @Test
+    public void testAssetsExist() {
+        try {
+            Assets.loadTextures();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
